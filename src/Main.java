2038b0275941342f0498f0f8af50fd6614afedbb
Davi Felipe <dev.davifelipe@gmail.com>
2025-06-05 21:38:12 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -17,7 +17,23 @@ import java.util.Scanner;
                 System.out.println("Quantity in stock: ");
                 product.quantity = sc.nextInt();
 
-                System.out.println(product.toString());
+                System.out.println();
+                System.out.println("Product data: " + product.toString());
+
+                System.out.println();
+                System.out.print("Enter the numer of products to be added in stock: ");
+                int quantity = sc.nextInt();
+                product.addProducts(quantity);
+
+                System.out.println();
+                System.out.print("Enter the numer of products to be removed from stock: ");
+                quantity = sc.nextInt();
+                product.removeProducts(quantity);
+                System.out.println();
+                System.out.println("Updated data: " + product);
+
+                    sc.close();
+                
 
 
 
